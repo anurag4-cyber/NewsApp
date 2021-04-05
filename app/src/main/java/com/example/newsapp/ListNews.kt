eedc73ac1a03bd44fa324da8f45c86ed2d38ce18
sunil-03 <73105533+sunil-03@users.noreply.github.com>
2021-04-05 16:06:43 +0530

    Create ListNews.kt

---
+++ app/src/main/java/com/example/newsapp/ListNews.kt
@@ -0,0 +1,13 @@
+package com.example.newsfeedapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListNews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_news)
+
+
+    }
+}
