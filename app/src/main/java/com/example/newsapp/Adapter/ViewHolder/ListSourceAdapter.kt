78ee39e326f24c18b77781676f32339b585738f2
Dondapati123 <81755449+Dondapati123@users.noreply.github.com>
2021-04-05 15:57:35 +0530

    Create ListSourceAdapter.kt

---
+++ app/src/main/java/com/example/newsapp/Adapter/ViewHolder/ListSourceAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.newsfeedapp.Adapter.ViewHolder
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsfeedapp.Interface.ItemClickListener
+import com.example.newsfeedapp.Model.WebSite
+import com.example.newsfeedapp.R
+
+public class ListSourceAdapter(private val context:Context,private val website:WebSite):RecyclerView.Adapter<ListSourceViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSourceViewHolder {
+       val inflater=LayoutInflater.from(parent!!.context)
+        val itemView=inflater.inflate(R.layout.source_news_layout,parent,false)
+        return ListSourceViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+       return website.sources!!.size
+    }
+
+    override fun onBindViewHolder(holder: ListSourceViewHolder, position: Int) {
+        holder!!.source_title.text=website.sources!![position].name
+        holder.setItemClickListener(object :ItemClickListener{
+            override fun onClick(view: View, position: Int) {
+                Toast.makeText(context,"will implement in future", Toast.LENGTH_SHORT).show();
+            }
+
+        })
+
+
+
+    }
+}
